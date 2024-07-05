86003bbb8a0b4d48ee7e5d68a6bd5318ef90bfb1
Tanmay Goraksha <140690246+Tanmay123Goraksha@users.noreply.github.com>
2024-07-05 15:25:43 +0530

    Add files via upload

--- trees.c
+++ trees.c
@@ -20,7 +20,7 @@ return n;
 }
 
 
-void preorder(struct Node * root){
+void preorder(struct node * root){
 
 if(root!= NULL)
 {
@@ -42,14 +42,129 @@ printf("%d",root->data);
 }
  }
 
-
 void inorder(){
 
 if(root! = NULL){
 
 inorder(root->left);
 printf("%d",root->data);
-inorder(root->right);
+inorder(ro ot->right);
+
+}
+
+}
+
+
+
+int isBST(struct node* root){
+
+    static struct node *prev = NULL;
+    if(root!=NULL){
+      if(isBST(root->left)){
+        return 0;
+      }
+      if(prev!=NULL && root->data <= prev->data){
+        return 0;
+      
+prev = root;
+return isBST(root->right);
+
+
+    }
+else{
+    return 1;
+}
+
+}
+}
+ 
+
+ struct node * searchrecursive(struct node* root, int key){
+if(root == NULL){
+    return NULL;
+}
+if(key == root->data){
+ return root;
+}
+else if(key<root->data){
+    return search(root->left,key);
+}
+else if(key>root->data){
+return search(root->right,key);
+}
+
+}
+
+struct node * searchiteratively(struct node* root, int key){
+
+while(root!=NULL){
+if(key == root->data)
+return root;
+}
+else if(key<root->data){
+    root = root ->left;
+}
+else if(key>root->data){
+    root = root ->right;
+}
+
+return NULL;
+
+}
+
+
+
+
+
+
+
+void insert(struct node *root, int key){
+
+struct node *prev = NULL;
+while(root!=NULL){
+prev = root;
+if(key == root->data){
+    return;
+}
+else if(key<root->data){
+ root = root->left;
+}
+else if(key>root->data){
+    root = root->right
+}
+
+}
+
+struct node* new = CreateNode(key);
+if(key<prev->data){
+    prev->left = new;
+}
+else{
+    prev->right = new;
+}
+
+
+
+
+}
+
+
+
+
+void delete(){
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
@@ -61,25 +176,21 @@ inorder(root->right);
 
 
 
-}
-
-
-
-
-
-
-
-
+ 
 
 
 int main(){
 
-struct node *p = CreateNode(2);
+struct node *p = CreateNode(5);
 struct node *p1 = CreateNode(3);
-struct node *p2 = CreateNode(4);
+struct node *p2 = CreateNode(6);
+struct node *p3 = CreateNode(1);
+struct node *p4 = CreateNode(4);
 
 p->left = p1;
 p->right = p2;
+p1->left = p3;
+p1->right = p4;  
 
 
 
